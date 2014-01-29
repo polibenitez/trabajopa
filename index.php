0401851d9a3e0d82c404e92fa62e42b171a89785
MAMISHO <mamisho@hotmail.es>
2014-01-29 03:44:27 +0100

    Pagina pirincipal
    El índex originar redirige a la pagina principal situada en la raíz de
    “principal”

--- index.php
+++ index.php
@@ -2,35 +2,9 @@
 <head>
 	<title>MUPOS</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta http-equiv="refresh" content="0;URL=principal/index.html">
 </head>
 <body>
-	<script>/*
-	var http_request = new XMLHttpRequest();
-	var url = "./datosLista.php"; // Esta URL debería devolver datos JSON
-	//alert(url);
-	http_request.onreadystatechange = manejaJSON;
-http_request.open("GET", url, true);
-http_request.send(null);
-
-function manejaJSON() {
-  if (http_request.readyState == 4) {
-    if (http_request.status == 200) {
-      var cadCodificadaJSON = http_request.responseText; 
-      var objDatos = eval("(" + cadCodificadaJSON + ")"); //Creamos el objeto utilizando la cadena codificada
-      //Ahora con el objeto desplegamos los datos mandados desde el servidor
-      /*document.getElementById("divId").innerHTML = objDatos.id;
-      document.getElementById("divNombre").innerHTML = objDatos.nombre;
-      document.getElementById("divCategoria").innerHTML = objDatos.categoria;
-      document.getElementById("divPrecio").innerHTML = objDatos.precioDeSalida;
-      document.getElementById("divDetalles").innerHTML = objDatos.detalles[0] + ", " + objDatos.detalles[1] + ", " + objDatos.detalles[2];*//*
-      //alert(objDatos.nombre);
-    } else {
-      alert("Ocurrio un problema con la URL.");
-    }
-    http_request = null;
-  }
-}*/
-	</script>
 	<div class="contenedor">
 		<header>
 			<div class="titulos">
@@ -42,7 +16,7 @@ function manejaJSON() {
 		</header>
 		<section>
 			<div class="mapa">
-				<iframe src="./principal/index.html" style="width:99%;height:99%;"></iframe>
+				
 			</div>
 		</section>
 		<footer>
