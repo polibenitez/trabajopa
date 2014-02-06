c1f3f90b78baf771226be6dbc153dfe1ab90d43a
zadik <zadikmccold@gmail.com>
2014-02-06 16:27:01 +0100

    principal de nuevo

---
+++ index.php
@@ -0,0 +1,13 @@
+ 	<<!DOCTYPE html>
+ 	<html>
+ 	<head>
+ 		<title>MUPOS</title>
+ 		<link rel="stylesheet" type="text/css" href="style.css">
++		<meta http-equiv="refresh" content="0;URL=principal/index.html">
+ 	</head>
+ 	<body>
+ 	
+ 	</body>
+ 	</html>
+ 	
+ 	
